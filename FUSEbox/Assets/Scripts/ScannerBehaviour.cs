205a8d2711c03e5c2cb4594b474620a53db5286d
craighughes58 <90798245+craighughes58@users.noreply.github.com>
2022-01-25 19:36:18 -0600

    Scanner part 1

--- FUSEbox/Assets/Scripts/ScannerBehaviour.cs
+++ FUSEbox/Assets/Scripts/ScannerBehaviour.cs
@@ -2,66 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using ZXing;
+
 public class ScannerBehaviour : MonoBehaviour
 {
-    //checks if the camera is currently available
-    private bool camAvailable;
-    //this is the information from the back camera
-    private WebCamTexture backCam;
-    //it defaults to this texture if the back camera is unavailable
-    private Texture defaultBackground;
-    [Tooltip("displays a texture2D UI element")]
-    public RawImage background;
-    [Tooltip("controls the aspect ratio manually")]
-    public AspectRatioFitter fit;
 
+
+    [SerializeField]
+    private RawImage RawImageBackground;
+    [SerializeField]
+    private AspectRatioFitter AspectRatioFilter;
+    [SerializeField]
+    private TextMeshProUGUI textOut;
+    [SerializeField]
+    private RectTransform scanZone;
+
+    private bool isCanvasAvailable;
+    private WebCamTexture CamTexture;
 
     // Start is called before the first frame update
     void Start()
     {
-        defaultBackground = background.texture;
-        WebCamDevice[] cameras = WebCamTexture.devices;
-
-        if(cameras.Length <= 0)
-        {
-            Debug.Log("NO CAMERAS");
-            camAvailable = false;
-            return;
-        }
-
-        for(int i = 0; i < cameras.Length; i++)
-        {
-            if(!cameras[i].isFrontFacing)
-            {
-                backCam = new WebCamTexture(cameras[i].name, Screen.width, Screen.height);
-            }
-        }
-        if(backCam == null)
-        {
-            Debug.Log("NO CAMERAS");
-            return;
-        }
-
-        backCam.Play();
-        background.texture = backCam;
-
-        camAvailable = true;
+        SetUpCamera();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if there's nothing to update then just end
-        if(!camAvailable)
+ 
+    }
+
+    private void SetUpCamera()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices; // looks for cameras on devices and puts them into a list
+        if(devices.Length == 0)//if there are no cameras end
         {
+            isCanvasAvailable = false;
             return;
         }
-        //set the aspect ratio to the camera's
-        fit.aspectRatio = (float)backCam.width / (float)backCam.height;
-        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
-        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
-
-        int oreintation = -backCam.videoRotationAngle;
-        background.rectTransform.localEulerAngles = new Vector3(0,0, oreintation);
+        for(int i = 0; i < devices.Length; i++) //if there's a front facing camera set it to the current camera
+        {
+            if (devices[i].isFrontFacing == false)
+            {
+                CamTexture = new WebCamTexture(devices[i].name, (int)scanZone.rect.width, (int)scanZone.rect.height);
+            }
+            CamTexture.Play();
+            RawImageBackground.texture = CamTexture;
+            isCanvasAvailable = true;
+        }
     }
 }
