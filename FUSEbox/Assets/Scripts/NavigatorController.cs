8786ef30104acaf789838560e80c7dc8e59e9425
craighughes58 <90798245+craighughes58@users.noreply.github.com>
2022-02-09 16:45:08 -0600

    scenes continued

--- FUSEbox/Assets/Scripts/NavigatorController.cs
+++ FUSEbox/Assets/Scripts/NavigatorController.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class NavigatorController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchScene(string nextScene)
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
     }
 }
